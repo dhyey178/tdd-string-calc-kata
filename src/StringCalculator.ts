41a80030f8dcb2ec63b008e4b0cd1ed46bd4f8b7
Dhyeya <dhyeya.shah178@gmail.com>
2025-06-06 08:47:21 +0530

    Implemented custom delimiters of any length

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -8,19 +8,23 @@ export class StringCalculator {
       const customDelimiterPart = parts[0];
       const actualNumbersPart = numbers.substring(numbers.indexOf('\n')+1)
 
-      const customDelimiter = customDelimiterPart.substring(2);
+      let customDelimiter;
+      if (customDelimiterPart.startsWith("//[")) {
+        customDelimiter =  customDelimiterPart.substring(3, customDelimiterPart.length-1);
+      } else {
+        customDelimiter = customDelimiterPart.substring(2);
+      }
+
       delimiters = new RegExp(`[\\n,${customDelimiter}]`);
       numbersToParse = actualNumbersPart;
     }
 
     const numberStrings = numbersToParse.split(delimiters);
-    const parsedNumbers: number[] = [];
     const negativeNumbers: number[] = [];
 
     let sum = 0;
     for (const numStr of numberStrings) {
       const num = parseInt(numStr, 10);
-      parsedNumbers.push(num);
       if (num < 0) {
         negativeNumbers.push(num);
       } else if (num <= 1000){
