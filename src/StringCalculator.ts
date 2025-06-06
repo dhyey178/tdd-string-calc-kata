bda0d63f142828e5262f65c235a3b2d2c75b5cac
Dhyeya <dhyeya.shah178@gmail.com>
2025-06-06 06:52:47 +0530

    Implemented throwing exception for negative numbers

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,9 +1,5 @@
 export class StringCalculator {
   Add(numbers: string): number {
-    if (numbers === "") {
-      return 0;
-    }
-
     let delimiters: RegExp | string[] = /[\n,]/;
     let numbersToParse = numbers;
 
@@ -11,16 +7,30 @@ export class StringCalculator {
       const parts = numbers.split('\n');
       const customDelimiterPart = parts[0];
       const actualNumbersPart = numbers.substring(numbers.indexOf('\n')+1)
+
       const customDelimiter = customDelimiterPart.substring(2);
       delimiters = new RegExp(`[\\n,${customDelimiter}]`);
       numbersToParse = actualNumbersPart;
     }
 
     const numberStrings = numbersToParse.split(delimiters);
+    const parsedNumbers: number[] = [];
+    const negativeNumbers: number[] = [];
 
-    const sum = numberStrings.reduce((acc, numStr) => {
-      return acc + parseInt(numStr, 10);
-    }, 0);
+    let sum = 0;
+    for (const numStr of numberStrings) {
+      const num = parseInt(numStr, 10);
+      parsedNumbers.push(num);
+      if (num < 0) {
+        negativeNumbers.push(num);
+      } else if (num >= 0){
+        sum += num;
+      }
+    }
+
+    if (negativeNumbers.length > 0) {
+      throw new Error(`negatives not allowed ${negativeNumbers.join(',')}`);
+    }
 
     return sum;
   }
