81eee348231d92835d3547a96d828fb60ae0c1a6
Dhyeya <dhyeya.shah178@gmail.com>
2025-06-06 06:27:54 +0530

    Implemented single character custom delimiters

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -4,10 +4,24 @@ export class StringCalculator {
       return 0;
     }
 
-    const numberStrings = numbers.split(/[\n,]/);
+    let delimiters: RegExp | string[] = /[\n,]/;
+    let numbersToParse = numbers;
+
+    if (numbers.startsWith("//")) {
+      const parts = numbers.split('\n');
+      const customDelimiterPart = parts[0];
+      const actualNumbersPart = numbers.substring(numbers.indexOf('\n')+1)
+      const customDelimiter = customDelimiterPart.substring(2);
+      delimiters = new RegExp(`[\\n,${customDelimiter}]`);
+      numbersToParse = actualNumbersPart;
+    }
+
+    const numberStrings = numbersToParse.split(delimiters);
+
     const sum = numberStrings.reduce((acc, numStr) => {
       return acc + parseInt(numStr, 10);
     }, 0);
+
     return sum;
   }
 }
